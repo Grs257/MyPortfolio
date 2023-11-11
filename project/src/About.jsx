e454c15935ef7f0cab96a2faa2a5686d3800ba68
BODDU GIRIRAJASEKHAR <116698450+Grs257@users.noreply.github.com>
2023-11-11 17:05:22 +0530

    added

--- project/src/About.jsx
+++ project/src/About.jsx
@@ -7,21 +7,32 @@ function About() {
           <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
               <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                   <div className="md:5/12 lg:w-5/12">
-                       <img src="https://images.unsplash.com/photo-1509023464722-18d996393ca8?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGphcGFufGVufDB8fDB8fHww" alt="imag" />
+                       <img src="" alt="imag" />
                   </div>
                   <div className="md:7/12 lg:w-6/12">
                       <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                          React development is carried out by passionate developers
+                          Education
                       </h2>
-                      <p className="mt-6 text-gray-600">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                          accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                          aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                      <p className="mt-0 text-gray-600">
+                           <div className='sides flex'><h2>Institute of Technolgy Guru Ghasidas Vishwavidyalaya </h2>
+                           <h2 className='ma pl-12'>2021-2025</h2></div> 
+                           <h2>Computer Science Engineering</h2>
+                           <p>8.02 G.P.A</p>
+                  <hr />
+                  <div className="na flex"><h2>Sri Chaitanya Junior Kalasala</h2>
+                  <h2 className='ma pl-56'>2019-2021</h2></div>
+                   
+                  <h2>Intermidiate</h2>
+                  <p>97.4%</p>
+                  <hr />
+                  <div className="ma flex"><h2>Bhashyam High School</h2>
+                   <h2 className='me pl-64'>2018-2019</h2></div>
+                    
+                  <h2>10th</h2>
+                  <p>97.4%</p>
                       </p>
-                      <p className="mt-4 text-gray-600">
-                          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                          Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                      </p>
+                  
+                   
                   </div>
               </div>
           </div>
--- project/src/Header.jsx
+++ project/src/Header.jsx
@@ -16,20 +16,21 @@ function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            to="https://github.com/Grs257"
+                            target='blank'
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Log in
+                           GitHub
                         </Link>
                         <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            to="https://www.figma.com/files/user/1242454591401173256?fuid=1242454591401173256"
+                            target='blank+'
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-grey-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
-                            Get started
+                           Figma
                         </Link>
                     </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
@@ -63,7 +64,7 @@ function Header() {
                                     Contact
                                 </NavLink>
                             </li>
-                            
+                                    
                             
                         </ul>
                     </div>
